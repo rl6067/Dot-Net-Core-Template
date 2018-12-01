0d49e4f02b85c3d65e7aa1b6d6243238d534b8aa
rl6067 <rl6067@yahoo.com>
2018-12-01 12:45:10 +0000

    Inital commit

---
+++ Web/FrontEnd/jsx/Containers/home.jsx
@@ -0,0 +1,6 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloWorld from '../Components/helloWorld.jsx'
+import '../../../FrontEnd/scss/styles.scss'
+
+ReactDOM.render(<HelloWorld message="Hello World" /> , document.getElementById('root'));
---
+++ Web/Startup.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Web.Infrastructure.Data;
+
+namespace Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.Configure<RazorViewEngineOptions>(o =>
+            {
+                // {2} is area, {1} is controller,{0} is the action    
+                o.ViewLocationFormats.Clear();
+                o.ViewLocationFormats.Add("~/Features/{1}/{0}" + RazorViewEngine.ViewExtension);
+                o.ViewLocationFormats.Add("~/Features/Shared/{0}" + RazorViewEngine.ViewExtension);
+                o.ViewLocationFormats.Add("../../Features/Shared/{0}" + RazorViewEngine.ViewExtension);
+                o.ViewLocationFormats.Add("~/Areas/{2}/Features/{1}/{0}" + RazorViewEngine.ViewExtension);
+
+            });
+
+
+            // automapper
+            services.AddAutoMapper(typeof(Startup).GetTypeInfo().Assembly);
+
+            // mediatR
+            services.AddMediatR(typeof(Startup).GetTypeInfo().Assembly);
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+                app.UseBrowserLink();
+
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+            {
+                HotModuleReplacement = true
+            });
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
